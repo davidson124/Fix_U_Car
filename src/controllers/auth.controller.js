59c502392d29aa12eb81ba84b2827ed791b6b9b0
davidson124 <daferdinat@gmail.com>
2026-02-07 17:19:17 -0500

    feat: auth routes connected and password hashing working

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,77 @@
+//It handles the logic of authentication requests (registration, login, logout).
+import User from '../models/User.model.js';
+import { hashPassword, comparePassword } from '../utils/hashPassword.js';
+
+export const login = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        //Basic validation
+        if(!email || !password){
+            return res.status(400).json({
+                msg: 'Email and password are required'
+            });
+        }
+        //Find user to email
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(404).json({
+                msg: 'User not found'
+            });
+        }
+        //Compare password
+        const isMatch = await comparePassword(password, user.password);
+        if(!isMatch){
+            return res.status(401).json({
+                msg: 'Invalid password'
+            });
+        }
+        res.status(200).json({
+            msg:'Login successful',
+        user:
+        {
+            id: user._id,
+            name: user.name,
+            role: user.role
+        } })
+    }catch(error){
+        console.error(error);
+        res.status(500).json({
+            msg: 'Server error'
+        });
+    }
+};
+
+export const register = async (req, res) => {
+    try{
+        const { name, email, password, cellphoneNumber, role } = req.body;
+        if(!name || !email || !password || !cellphoneNumber ){
+            return res.status(400).json({
+                msg:'All fields are required'
+            });
+        }
+        const userExists = await User.findOne({ email });
+        if(userExists){
+            return res.status(400).json({
+                msg:'User already exists'
+            });
+        }
+        const hashedPassword = await hashPassword(password);
+
+        const user = new User ({
+            name,
+            email,
+            password: hashedPassword,
+            cellphoneNumber,
+            role
+        });
+        await user.save();
+        res.status(201).json({
+            msg:'User registered successfully'
+        });
+    }catch(error){
+        console.error(error);
+        res.status(500).json({
+            msg: 'Server error'
+        });
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import authRoutes from './routes/auth.routes.js';
 import User from './models/User.model.js'
 
-
+dotenv.config();
 const app = express();
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
 connectDB();
 
 app.use(express.json());
+
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server running on http://localhost:${PORT}`);
